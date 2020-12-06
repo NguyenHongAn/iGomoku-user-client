9a53c7d3587dec09d6fefc663d3fca2f0aa91d23
Nguyen Hong An <37919921+NguyenHongAn@users.noreply.github.com>
2020-12-06 13:22:13 +0700

    [Init] Initial project with redux and boststrap

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {connect } from 'react-redux';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/'>
+          <h1>Home page</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const mapState = (state) =>({
+  ...state.socketReducer
+});
+
+const mapDispatch = {
+  log: console.log("dispatch")
+};
+
+export default connect(
+  mapState,
+  mapDispatch
+)(App);
